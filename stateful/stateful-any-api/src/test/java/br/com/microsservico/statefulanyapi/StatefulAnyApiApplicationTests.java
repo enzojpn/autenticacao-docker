6cbb5362c51d967d8bcd3b3e50e33901ce9ef6d7
enzojpn <enzo_jpn@hotmail.com>
2023-11-03 23:59:14 -0300

    spring boot api

---
+++ stateful/stateful-any-api/src/test/java/br/com/microsservico/statefulanyapi/StatefulAnyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microsservico.statefulanyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatefulAnyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ stateless/stateless-any-api/src/test/java/br/com/microsservico/statelessanyapi/StatelessAnyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microsservico.statelessanyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatelessAnyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
